4fe643e6c72387bbd93bf0eed50c198f64c3226a
Praveen Yadav <apple@Admins-MacBook-Pro-2.local>
2025-11-24 18:33:46 +0530

    Google AdSence ad

---
+++ src/components/AdUnit.jsx
@@ -0,0 +1,19 @@
+{/* import { useEffect } from "react";
+
+export default function AdUnit() {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (err) {}
+  }, []);
+
+  return (
+    <ins className="adsbygoogle"
+      style={{ display: "block", textAlign: "center", margin: "20px auto" }}
+      data-ad-client="ca-pub-7180154058776132"
+      data-ad-slot="1234567890"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+  );
+} */}
